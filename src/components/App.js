6bb4590b68a01a61c3e325f4302740cfad25572b
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-08-21 12:07:06 +0500

    set up iframe

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ function App() {
     <>
       <div className="pane top-pane"></div>
       <div className="pane">
-        <iframe title="Output" sandbox="allow-scripts" />
+        <iframe
+          title="Output"
+          sandbox="allow-scripts"
+          frameBorder={0}
+          width="100%"
+          height="100%"
+        />
       </div>
     </>
   );
